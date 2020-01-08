f566e7ccdeb81a864d7cfdf1616ef2976a8c916e
Jaylon Gowie <40341589+SonicMastr@users.noreply.github.com>
2020-01-07 18:19:48 -0600

    Updated to no longer use API (Client Side)
    Did this due to recommendation and crashing of API

--- index.lua
+++ index.lua
@@ -1,4 +1,17 @@
 Network.init()
+
+json = dofile("ux0:/app/RAND00001/deps/json.lua")
+h,m,s = System.getTime()
+math.randomseed(s)
+
+if Network.isWifiEnabled() then
+	Network.downloadFile("https://konachan.com/post.json?limit=250&tags=+-furry%20order:score%20rating:explict", "ux0:/data/post.json")
+	file = System.openFile("ux0:/data/post.json", FREAD)
+	size = System.sizeFile(file)
+	jsonEncoded = System.readFile(file, size)
+	jsonDecoded = json.decode(jsonEncoded)
+end
+
 ::gethentai::
 
 if Network.isWifiEnabled() then
@@ -6,9 +19,11 @@ if Network.isWifiEnabled() then
 		Graphics.freeImage(img)
 		img = nil
 	end
-	Network.downloadFile("http://api.jaylongowie.tk:3000/hentai", "ux0:/data/randomhentai.png")
-	img = Graphics.loadImage("ux0:/data/randomhentai.png")
-	System.deleteFile("ux0:/data/randomhentai.png")
+	rand = math.random(#jsonDecoded)
+	url = jsonDecoded[rand]["jpeg_url"]
+	Network.downloadFile(url, "ux0:/data/randomhentai.jpg")
+	img = Graphics.loadImage("ux0:/data/randomhentai.jpg")
+	System.deleteFile("ux0:/data/randomhentai.jpg")
 	width = Graphics.getImageWidth(img)
 	height = Graphics.getImageHeight(img)
 	drawWidth = 480 - (width * 544 / height / 2)
@@ -34,9 +49,9 @@ while true do
 	end
 	Graphics.termBlend()
 	Screen.flip()
+	Screen.waitVblankStart()
 	
 	if Controls.check(Controls.read(), SCE_CTRL_CROSS) then
-		System.wait(1000000)
 		goto gethentai
 	end
 	
